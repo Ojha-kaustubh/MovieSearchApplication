d9305edd6b4cdd1feebce3b913cf5c888d8ffc13
Kaustubh Ojha <166204250+Ojha-kaustubh@users.noreply.github.com>
2024-11-19 23:30:57 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-
 const useFetch = (endpoint)=>{
     const [data,setData] = useState([])
     const [loading,setLoading] = useState(false)
